d0d2c1155039c50ebd7d7d695474db6f392cf42a
Abdelrhman131 <abdelrhmanmagdy08@gmail.com>
2023-07-20 19:52:35 -0700

    lklki

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if input is digit between 0 - 9
+ *
+ * @c: input
+ *
+ * Return: 1 if is digit, 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
